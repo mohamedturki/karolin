b55ecf71a51d65a091f6db38773c9a57e827f8a9
Mohamed Turki <turkipro@gmail.com>
2015-12-06 20:18:51 +0100

    Remove nginx and gunicorn from development Add docker compose config file for production Fix static files finders

--- web/entrypoint-production.sh
+++ web/entrypoint-production.sh
@@ -5,8 +5,10 @@
 while ! nc -z db 5432; do sleep 3; done
 python manage.py migrate
 
+python manage.py collectstatic --noinput
+
 # Create a superuser.
 # Credits go to: http://source.mihelac.org/2009/10/23/django-avoiding-typing-password-for-superuser/
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('karolina', 'k.dziorek@gmail.com', 'rootpassword')" | python manage.py shell
 
-python manage.py runserver 0000:8000
+/usr/local/bin/gunicorn karolin.wsgi:application -w 2 -b :8000
