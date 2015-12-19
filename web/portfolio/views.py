88f99c098756377a92949e93aa1f8cf62ac7ea6d
Mohamed Turki <turkipro@gmail.com>
2015-12-19 20:06:37 +0100

    Add active urls

--- web/portfolio/views.py
+++ web/portfolio/views.py
@@ -12,9 +12,21 @@ class ProjectList(ListView):
     template_name = 'portfolio/layouts/index.html'
     context_object_name = 'projects'
 
+    def get_context_data(self, **kwargs):
+        context = super(ProjectList, self).get_context_data(**kwargs)
+        context['categories'] = ProjectCategory.objects.all()
+
+        return context
+
 
 class RecentProjectList(ProjectList):
     queryset = Project.objects.order_by('pub_date')[:4]
+
+    def get_context_data(self, **kwargs):
+        context = super(RecentProjectList, self).get_context_data(**kwargs)
+        context['categories'] = ProjectCategory.objects.all()
+
+        return context
 
 
 class ProjectDetail(DetailView):
@@ -26,5 +38,7 @@ class ProjectDetail(DetailView):
         context['similar_projects'] = Project.objects.filter(
             category=self.object.category
         ).exclude(slug=self.object.slug)[:3]
+
+        context['categories'] = ProjectCategory.objects.all()
 
         return context
