f972ba1a52e3431e8112aa7d59e01712af5110da
Mohamed Turki <turkipro@gmail.com>
2016-01-10 19:28:38 +0100

    Add media context processor

--- web/karolin/urls.py
+++ web/karolin/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('portfolio.urls', namespace="portfolio")),
     url(r'^markdown', include('django_markdown.urls')),
     url(r'^dashboard/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
