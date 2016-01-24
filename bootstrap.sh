5fb820f0bf747cd563f9e7c24698eae4152fc9fd
Mohamed Turki <turkipro@gmail.com>
2016-01-24 19:07:08 +0100

    Hide aws keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ docker-machine create -d digitalocean --digitalocean-access-token=$DIGITALOCEAN_
                         --engine-env DB_USER=postgres \
                         --engine-env DEBUG=False \
                         --engine-env DJANGO_SETTINGS_MODULE=karolin.settings.production \
-                        --engine-env AWS_ACCESS=AKIAINZULEHYYVVXAMDA \
-                        --engine-env AWS_SECRET=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 \
-                        --engine-env SECRET_KEY=Vtz5T21QNi0jNoLqLtX3i0O6EfrnHN \
+                        --engine-env AWS_ACCESS=$AWS_ACCESS \
+                        --engine-env AWS_SECRET=AWS_SECRET \
                         $MachineName
--- web/entrypoint-production.sh
+++ web/entrypoint-production.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export SECRET_KEY=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c30)
+export DJANGO_SETTINGS_MODULE="karolin.settings.production"
+
+
 # Waits for the Postgresql services to be available
 # before running Django migration.
 while ! nc -z db 5432; do sleep 3; done
