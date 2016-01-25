25892bd86bdf1075408d96dd14708f07be4a5d0c
Mohamed Turki <turkipro@gmail.com>
2016-01-25 11:26:50 +0100

    Change output text color

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 # Build and push images to Docker registry.
 #
 
-echo -e "Building Web image for $1\e[32mGreen"
+echo -e '\e[32mGreen' "Building Web image for $1"
 docker build -f web/Dockerfile -t mturki/karolin_web_$1 web
 docker push mturki/karolin_web_$1
 
-echo -e "Building Nginx image for $1\e[32mGreen"
+echo -e '\e[32mGreen' "Building Nginx image for $1"
 docker build -f nginx/Dockerfile -t mturki/karolin_nginx_$1 nginx
 docker push mturki/karolin_nginx_$1
